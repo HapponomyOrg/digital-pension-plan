a26cd13f414edfcfe5770811d101610a1ca4cbe6
Luuk van Es <Luuk.van.es@outlook.com>
2025-12-03 23:24:50 +0100

    Added base for new phase system

---
+++ Assets/Version1/Phases/PhaseLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Version1.Phases
+{
+    public static class PhaseLibrary
+    {
+        public static Phase MarketPhase { get; } = new("Market", "MarketScene");
+        public static Phase LoadingPhase { get; } = new("Loading", "Loading");
+        public static Phase MoneyCorrectionPhase { get; } = new("MoneyCorrection", "MoneyCorrectionScene");
+        public static Phase MoneyToPointPhase { get; } = new("MoneyToPoint", "MoneyToPointScene");
+        public static Phase DonatePointsPhase { get; } = new("DonatePoints", "DonatePointsScene");
+        public static Phase PayDebtPhase { get; } = new("PayDebt", "PayDeptScene");
+        public static Phase TakeALoanPhase { get; } = new("TakeALoan", "TakeALoanScene");
+        public static Phase EndPhase { get; } = new("End", "EndScene");
+    }
+}
